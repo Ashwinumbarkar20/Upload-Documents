60ecf06d9708ce098e666d369da36762af206beb
Ashwin <Ashwin@vridhee.com>
2025-04-16 23:12:37 +0530

    added remove funtionlity and other styles for images

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,14 @@ const handlefilechange=(e)=>{
 const files=e.target.files
 console.log([...files])
 
-  setFileList(files)
+  setFileList([...fileList,...files])
 
   }
-
+const handleDelete=(filename)=>{
+const updatedData=fileList.filter((file)=>file.name!==filename)
+console.log("updatedData",updatedData)
+setFileList(updatedData)
+}
 
   return (
    <>
@@ -23,23 +27,23 @@ console.log([...files])
 <div><label style={{fontWeight:"700",fontSize:"18px",cursor:"pointer"}} htmlFor="UploadDocument">Upload Document</label>
 <input onChange={handlefilechange} style={{display:"none"}} type="file" multiple id="UploadDocument" /></div>
 </div>
-
 </div>
 <div className="ListofDocumentsUpload"><h3 style={{textAlign:"center"}}>List of Documents</h3>
 <div>
 {
-  fileList.length>0 ? (<>
+  fileList.length>0 ? (<div className="Doclist">
 {
-  fileList.map((file)=><>
-  <img src={URL.createObjectURL(file)} alt={file.name} />
-  <span>{file.name}</span>
-  <span>{file.size/1025}Kb</span>
-  <span>{file.name}</span>
+  fileList.map((file,i)=>
+  <div key={i} className="FileDetails">
+  <img style={{width:"75px",height:"75px",border:"2px solid black",borderRadius:"5px"}}src={URL.createObjectURL(file)} alt={file.name} />
+  <span>{file.name.substring(0,15)+"..."}</span>
+  <span>{(file.size/1025).toFixed(2)}Kb</span>
+  <span style={{padding:"2px",textAlign:"center",color:"white",cursor:"Pointer",background:"red",width:"20px",height:"20px",borderRadius:"5px"}} onClick={()=>handleDelete(file.name)}>{"X"}</span>
 
-  </>)
+  </div>)
 }
 
-  </>):"No Data"
+  </div>):"No Data"
 }
 
 
